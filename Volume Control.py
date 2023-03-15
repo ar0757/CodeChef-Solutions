5a28cb374d341e6f33c8c73614a3d1ebe6b55368
ar0757 <111003551+ar0757@users.noreply.github.com>
2023-03-15 17:55:40 +0400

    Create Volume Control.py

---
+++ Volume Control.py
@@ -0,0 +1,9 @@
+t = int(input())
+while(t>0):
+    l =list(map(int,input().split()))
+    print(abs(l[0]-l[1]))
+    t -= 1t = int(input())
+while(t>0):
+    l =list(map(int,input().split()))
+    print(abs(l[0]-l[1]))
+    t -= 1
