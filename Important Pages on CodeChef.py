2b6cdd46cf1b0aaee977448a188f32011caed216
ar0757 <111003551+ar0757@users.noreply.github.com>
2023-03-31 16:44:09 +0400

    Create Important Pages on CodeChef.py

---
+++ Important Pages on CodeChef.py
@@ -0,0 +1,8 @@
+# cook your dish here
+a,b=map(int,input().split())
+if a==1 and b==1 :
+    print("https://discuss.codechef.com")
+elif a==1 and b==0:
+    print("https://www.codechef.com/contests")
+else:
+    print("https://www.codechef.com/practice")
